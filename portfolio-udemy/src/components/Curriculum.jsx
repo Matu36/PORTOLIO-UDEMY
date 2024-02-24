5c4e7bd0c2c8bb1016b00e57c6d20252e7578b2f
Matu36 <matipineda85@live.com.ar>
2024-02-24 14:59:15 -0300

    terminado

--- portfolio-udemy/src/components/Curriculum.jsx
+++ portfolio-udemy/src/components/Curriculum.jsx
@@ -9,59 +9,44 @@ export default function Curriculum() {
       <p>Especializada en NODE.JS, Express, Sequelize y PostgreSQL</p>
       <p>Graduada de la Facultad de Sistemas de la UNLP</p>
 
-      <section>
-        <h2>Experiencia</h2>
-        <ul>
-          <li>
-            <strong>Desarrollador Backend</strong> - [Nombre de la Empresa] -
-            [Ubicación] - [Fecha de Inicio] a [Fecha de Fin]
-            <ul>
-              <li>
-                Trabajé en el desarrollo y mantenimiento de sistemas backend
-                utilizando tecnologías como NODE.JS, Express, Sequelize y
-                PostgreSQL.
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </section>
+      <br />
+      <h2>Experiencia</h2>
 
-      <section>
-        <h2>Conocimientos</h2>
-        <ul>
-          <li>NODE.JS</li>
-          <li>Express</li>
-          <li>Sequelize</li>
-          <li>PostgreSQL</li>
-        </ul>
-      </section>
+      <p>
+        <strong>Desarrollador Backend</strong> - [Nombre de la Empresa] -
+        [Ubicación] - [Fecha de Inicio] a [Fecha de Fin]
+      </p>
+      <p>
+        {" "}
+        Trabajé en el desarrollo y mantenimiento de sistemas backend utilizando
+        tecnologías como NODE.JS, Express, Sequelize y PostgreSQL.
+      </p>
+      <br />
+      <h2>Conocimientos</h2>
 
-      <section>
-        <h2>Educación</h2>
-        <ul>
-          <li>
-            <strong>Facultad de Sistemas</strong> - Universidad Nacional de La
-            Plata (UNLP) - [Fecha de Graduación]
-          </li>
-        </ul>
-      </section>
+      <p>NODE.JS </p>
+      <p>Express </p>
+      <p>Sequelize </p>
+      <p>PostgreSQL </p>
 
-      <section>
-        <h2>Proyectos Personales</h2>
-        <ul>
-          <li>
-            <strong>Nombre del Proyecto</strong> - [Descripción breve del
-            proyecto] - [Fecha de Inicio] a [Fecha de Fin]
-          </li>
-        </ul>
-      </section>
+      <br />
+      <h2>Educación</h2>
 
-      <section>
-        <h2>Reconocimientos y Premios</h2>
-        <ul>
-          <li>[Nombre del Reconocimiento/Premio] - [Fecha de Recepción]</li>
-        </ul>
-      </section>
+      <p>
+        <strong>Facultad de Sistemas</strong> - Universidad Nacional de La Plata
+        (UNLP) - [Fecha de Graduación]
+      </p>
+      <br />
+      <h2>Proyectos Personales</h2>
+
+      <p>
+        {" "}
+        <strong>Nombre del Proyecto</strong> - [Descripción breve del proyecto]
+        - [Fecha de Inicio] a [Fecha de Fin]
+      </p>
+      <br />
+      <h2>Reconocimientos y Premios</h2>
+      <p>[Nombre del Reconocimiento/Premio] - [Fecha de Recepción]</p>
     </div>
   );
 }
